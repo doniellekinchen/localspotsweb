4a3f685b2c775230f59c9b4c1dfbac738a897bc2
doniellekinchen <donicodes.things@gmail.com>
2025-08-30 18:16:00 -0400

    fix navbar issue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue' // or wherever it’s stored
+import NavBar from './components/NavBar.vue' // or wherever it’s stored
 
 export default {
   name: 'App',
